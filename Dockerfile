c4262c178a29e0c4f22f1735417d101125294598
vfolgosa <viniciusrf@gmail.com>
2018-07-25 20:07:44 -0300

    Acerto do Dockerfile para o pipeline do  buddy e deploy na AWS

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,22 @@
 FROM openjdk:8-alpine
 FROM maven:alpine
 
+ENV dir /tmp
+
+VOLUME ${dir}
+
 MAINTAINER Vinicius Folgosa
 
-WORKDIR /code
+ADD . ${dir}
 
-ADD pom.xml /code/pom.xml
+WORKDIR ${dir} 
+
 RUN ["mvn", "dependency:resolve"]
 RUN ["mvn", "verify"]
-
-# Adding source, compile and package into a fat jar
-ADD src /code/src
 RUN mvn package -DskipTests=true
 
 EXPOSE 8080
 
-COPY target/*.jar app.jar
+ADD target/planets-0.0.1-SNAPSHOT.jar app.jar
 
 CMD ["java", "-jar", "-Dspring.profiles.active=prod" ,"./app.jar"]
--- src/main/java/br/com/viniciusrf/planets/PlanetsApplication.java
+++ src/main/java/br/com/viniciusrf/planets/PlanetsApplication.java
@@ -1,34 +1,30 @@
 package br.com.viniciusrf.planets;
 
-import java.util.concurrent.Executor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableAsync
+@EnableAsync 
 public class PlanetsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanetsApplication.class, args);
 	}
 	
-	 /*@Bean
+	 /*@Beanc
 	  public Executor asyncExecutor() {
 	    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 	    executor.setCorePoolSize(2);
 	    executor.setMaxPoolSize(5);
 	    executor.setQueueCapacity(500);
-	    executor.initialize();
+	    executor.initialize(); 
 	    return executor;
 	  }*/
 	  
